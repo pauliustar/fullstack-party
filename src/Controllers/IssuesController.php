267c1664f0822bd0cfaf7311967b10ac1b801c40
Paulius <paulius.taraskevicius@gmail.com>
2018-05-27 17:18:58 +0300

    Added Closed/Open Issues counter

--- src/Controllers/IssuesController.php
+++ src/Controllers/IssuesController.php
@@ -28,6 +28,8 @@ class IssuesController extends AuthController
     public function showIssues($request, $response, $args)
     {
         if (isset($_SESSION['access_token'])) {
+            $_SESSION['openIssues'] = 0;
+            $_SESSION['closedIssues'] = 0;
             $userInfo = $this->getUserInfo($request, $response, $args);
             $allLinks = $this->getRepos($request, $response, $args);
             $getIssues = new Curl();
@@ -43,20 +45,28 @@ class IssuesController extends AuthController
                 } else {
                     $githubResponse = $getIssues->response;
                     foreach ($githubResponse as $singleResponse) {
-                        $issues[] = [
-                          'url' => $singleResponse->url,
-                          'user' =>$singleResponse->user->login,
-                          'labels' => $singleResponse->labels_url,
-                          'comments' => $singleResponse->comments_url,
-                          'title' => $singleResponse->title,
-                          'body' => $singleResponse->body,
-                          'state' => $singleResponse->state
-                        ];
+                        if ($singleResponse->state != 'closed') {
+                            $issues[] = [
+                              'url' => $singleResponse->url,
+                              'id' => $singleResponse->id,
+                              'user' =>$singleResponse->user->login,
+                              'labels' => $singleResponse->labels,
+                              'comments' => $singleResponse->comments,
+                              'title' => $singleResponse->title,
+                              'body' => $singleResponse->body,
+                              'created' =>$singleResponse->created_at,
+                            ];
+                            $_SESSION['openIssues']++;
+                        } else {
+                            $_SESSION['closedIssues']++;
+                        }
                     }
                 }
             }
             return $this->container->view->render($response, 'issues.twig', $args = [
-              'issues' => $issues
+              'issues' => $issues,
+              'openIssues' => $_SESSION['openIssues'],
+              'closedIssues' => $_SESSION['closedIssues']
             ]);
         } else {
             return $this->container->view->render($response, 'index.twig', $args);
